41f37f48166507bc64a32a07532cdcd500d18d50
Mohsen Banan <git@mohsen.1.banan.byname.net>
2020-12-11 10:23:31 -0800

    Initial load.

--- android/ftoProc.sh
+++ android/ftoProc.sh
@@ -16,7 +16,7 @@ SEED="
 *  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
 "
 FILE="
-*  /This File/ :: /bisos/panels/bisos-periphery/ftoProc.sh 
+*  /This File/ :: /bisos/panels/bisos-periphery/android/ftoProc.sh 
 "
 if [ "${loadFiles}" == "" ] ; then
     /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
@@ -53,9 +53,6 @@ _CommentEnd_
 ####+BEGIN: bx:dblock:ploneProc:bash:nodesList :types ""
 # {{{ DBLOCK-nodesList
 nodesList="
-android
-_nodeBase_
-printingAndImaging
 "
 # }}} DBLOCK-nodesList
 ####+END:
